74fe7cd0f91656aeba52f524a0cd740532abaeaf
krakelin <drsounds@gmail.com>
2011-05-31 07:30:03 +0200

    Signed-off-by: krakelin <drsounds@gmail.com>

---
+++ MediaChrome/MediaChromeGUI/ListP.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Drawing.Design;
+namespace MediaChrome
+{
+    class ListViewX : ListView
+    {
+    
+        public ListViewX()
+        {
+            
+            this.OwnerDraw = false;
+            this.DrawItem += new DrawListViewItemEventHandler(ListViewX_DrawItem);
+            this.DrawSubItem += new DrawListViewSubItemEventHandler(ListViewX_DrawSubItem);
+            this.DrawColumnHeader += new DrawListViewColumnHeaderEventHandler(ListViewX_DrawColumnHeader);
+            this.BackColor = Color.FromArgb(64, 64, 64);
+            this.ForeColor = Color.White;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.SizeChanged += new EventHandler(ListViewX_SizeChanged);
+            this.OwnerDraw = false;
+            
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            
+        }
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+        	
+        }
+        void ListViewX_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.Columns.Count > 0)
+            {
+                this.Columns[0].Width = this.Width;
+            }
+        }
+
+        void ListViewX_DrawSubItem(object sender, DrawListViewSubItemEventArgs e)
+        {
+            int left = 0;
+            if (e.Item.Text != "-")
+            {
+                if (e.ColumnIndex == 0)
+                {
+                    left = 18;
+                    object key;
+                    key = (e.Item.ImageKey != null ? e.Item.ImageKey : "");
+                    if (key == "")
+                    {
+                        key = -1;
+                    }
+                    try
+                    {
+                        e.Graphics.DrawImage(this.SmallImageList.Images[key.ToString()], e.Bounds.Left, e.Bounds.Top);
+                    }
+                    catch
+                    {
+                    }
+                }
+                else
+                {
+                    left = 0;
+                }
+                if (e.Item.Selected)
+                {
+                    e.Graphics.DrawString(e.Item.Text, new Font("MS Sans Serif", 8), new SolidBrush(Color.Black), new Point(e.Bounds.Left+left, e.Bounds.Top));
+                }
+                else
+                {
+                    e.Graphics.DrawString(e.Item.Text, new Font("MS Sans Serif", 8), new SolidBrush(Color.White), new Point(e.Bounds.Left + left, e.Bounds.Top));
+                }
+            }
+            
+        }
+
+        void ListViewX_DrawColumnHeader(object sender, DrawListViewColumnHeaderEventArgs e)
+        {
+         
+        }
+
+        void ListViewX_DrawItem(object sender, DrawListViewItemEventArgs e)
+        {
+            if (e.Item.Text == "-")
+            {
+                e.Graphics.DrawLine(new Pen(Color.FromArgb(50,50,50)), new Point(e.Bounds.Left, e.Bounds.Top + e.Bounds.Height / 2), new Point(e.Bounds.Width, e.Bounds.Top + e.Bounds.Height / 2));
+            }
+            if (e.Item.Selected)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(127, 200, 255)), e.Bounds);
+            }
+        }
+
+    }
+}
---
+++ MediaChrome/MediaChromeGUI/ServicesSelector/Engines.designer.cs
@@ -0,0 +1,57 @@
+﻿namespace MediaChrome
+{
+    partial class Engines
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Engines));
+            this.SuspendLayout();
+            // 
+            // Engines
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(316, 321);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Engines";
+            this.ShowInTaskbar = false;
+            this.Text = "Engines";
+            this.TopMost = true;
+            this.Deactivate += new System.EventHandler(this.Engines_Deactivate);
+            this.Load += new System.EventHandler(this.Engines_Load);
+            this.Leave += new System.EventHandler(this.Engines_Leave);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
